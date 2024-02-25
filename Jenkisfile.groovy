ab716a3c138adcaed969c0e79a4f751b070da95a
JoseA <jducallin27@gmail.com>
2024-02-25 20:03:36 -0300

    guardado correcto de el archivo jenkins.groovy

---
+++ Jenkisfile.groovy
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Compile Stage') {
+            steps {
+                script {
+                    def mvnHome = tool name: 'maven_3_9_5', type: 'maven'
+                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
+                        bat "${mvnHome}\\bin\\mvn clean compile"
+                    }
+                }
+
+            }
+        }
+
+        stage('Test Stage') {
+            steps {
+                script {
+                    def mvnHome = tool name: 'maven_3_9_5', type: 'maven'
+                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
+                        bat "${mvnHome}\\bin\\mvn clean verify -Dcucumber.filter.tags=\"@SETDEPRUEBA\""
+                    }
+                }
+            }
+
+        }
+
+        stage('Cucumber Reports') {
+            steps {
+                cucumber buildStatus: "UNSTABLE",
+                        fileIncludePattern: "**/cucumber.json",
+                        jsonReportDirectory: 'target'
+            }
+        }
+
+    }
+
+}
